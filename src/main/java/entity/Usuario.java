e92c63fb64f1225cb95fba75efd91f1539387f9a
[Mateo Fares] <faresmateo2005@gmail.com>
2026-03-14 15:33:46 -0300

    agrego primer entidad y repository, comento el spring security

--- src/main/java/entity/Usuario.java
+++ src/main/java/entity/Usuario.java
@@ -1,4 +1,25 @@
 package entity;
 
+import enums.UserRol;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private UserRol userRol;
+    private String nombre;
+    private String mail;
+    private String contrasenia;
+    private String apellido;
+
 }
